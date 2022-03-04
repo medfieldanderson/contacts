1dc22fe2d45da7fa088bd5cd5826e7af1cffcd18
Jeff <medfield.anderson@gmail.com>
2022-03-04 15:38:00 -0500

    map contacts from json to components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,7 @@ const Card = (props) => {
   return (
     <div className="card">
       <div className="top">
+        <p>{props.id}</p>
         <h2 className="name">{props.name}</h2>
         <Avatar name={props.name} imgURL={props.imgURL} />
       </div>
